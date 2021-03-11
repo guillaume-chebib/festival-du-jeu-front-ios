efc6a6ceabfa667a952bc30b2f6b5a9d0fc18f5a
Guillaume C <guillaume.chebib@hotmail.fr>
2021-03-11 11:55:34 +0100

    Initial Commit

---
+++ festival-du-jeu-front-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  festival-du-jeu-front-ios
+//
+//  Created by user190184 on 11/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ festival-du-jeu-front-ios/festival_du_jeu_front_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  festival_du_jeu_front_iosApp.swift
+//  festival-du-jeu-front-ios
+//
+//  Created by user190184 on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct festival_du_jeu_front_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
