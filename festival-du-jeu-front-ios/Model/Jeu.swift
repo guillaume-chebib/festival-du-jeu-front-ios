864bf79e4ff5f023d4ed3e5ce1ea42e8bd4af893
Garira MOUNDI <mazougarira@yahoo.fr>
2021-03-23 10:49:45 +0100

    Create architecture

---
+++ festival-du-jeu-front-ios/Model/Jeu.swift
@@ -0,0 +1,8 @@
+//
+//  JeuModel.swift
+//  festival-du-jeu-front-ios
+//
+//  Created by etud on 23/03/2021.
+//
+
+import Foundation
---
+++ festival-du-jeu-front-ios/ViewModel/JeuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JeuViewModel.swift
+//  festival-du-jeu-front-ios
+//
+//  Created by etud on 23/03/2021.
+//
+
+import Foundation
